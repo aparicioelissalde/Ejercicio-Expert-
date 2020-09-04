3002e36150e67c27dfcbf85d9d542408b26cd470
aparicioelissalde <aparicio.elissalde@correo.ucu.edu.uy>
2020-09-04 19:53:21 -0300

    Ejercicio 3

---
+++ Ejercicio 2/src/Library/Libro.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// El método de almacenar libros no tendría que ir en la clase Libro 
+/// </summary>
+namespace SRP
+{
+    public class Libro
+    {
+
+        public string Titulo { get ; }
+        public string Autor { get ; }
+        public string Codigo { get ;  }
+        public string SectorBiblioteca { get ; set; }
+        public string EstanteBiblioteca { get ; set; }
+
+        public Libro(String titulo, String autor, String codigo)
+        {
+            this.Titulo = titulo;
+            this.Autor = autor;
+            this.Codigo = codigo;
+        }
+
+        public void AlmacenarLibro(String sector, String estante)
+        {
+            this.SectorBiblioteca = sector;
+            this.EstanteBiblioteca = estante;
+        }
+
+    }
+}
---
+++ Ejercicio 3/src/Program/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Program
+{
+/*    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    */
+    class Paciente
+    {
+        public string Nombre {get; set;}
+        public string Apellido {get; set;}
+        public string Edad {get; set;}
+
+
+        public Paciente(String nombre, String apellido, String edad)
+        {
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.Edad = edad;
+        }
+    }
+
+    class Doctor
+    {
+        public string Especialidad {get; set;}
+        public string Nombre {get; set;}
+        public string Apellido {get; set;}
+        public Doctor(String nombre, String apellido, String especialidad)
+
+
+        public Doctor(String nombre, String apellido, String especialidad)
+        {
+        this.Nombre = nombre;
+        this.Apellido = apellido;
+        this.Especialidad = especialidad;
+        }
+
+
+    }
+
+    class 
+}
